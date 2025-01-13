9ebf88902c763360b1ff8a5d154b23f2b383d117
Souvik Sarkar <sounix000@gmail.com>
2025-01-13 16:48:59 +0530

    Trivial update

--- build_git_routine.sh
+++ build_git_routine.sh
@@ -5,7 +5,7 @@ asciidoctor -o ./build/html/RESTvsSOAP.html ./src/RESTvsSOAP.adoc
 
 # Build pdf files using asciidoctor-pdf
 asciidoctor -r asciidoctor-pdf -b pdf -o ./build/pdf/exercises/findNeedles.pdf ./src/findNeedles.adoc
-asciidoctor -r asciidoctor-pdf -b pdf -o ./build/pdf/RESTvsSOAP.pdf ./src/RESTvsSOAP.adoc
+asciidoctor -r asciidoctor-pdf -b pdf -o ./build/pdf/exercises/RESTvsSOAP.pdf ./src/RESTvsSOAP.adoc
 asciidoctor -r asciidoctor-pdf -b pdf -o ./build/pdf/exercises/TicTacToe.pdf ./src/TicTacToe.adoc
 
 # Move TicTacToe.pdf to the samples folder
