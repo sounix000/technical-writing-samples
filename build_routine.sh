bfbda2861b5ba4887bc7f0ec333ef75ea93385b3
Souvik Sarkar <souvik.sarkar@suse.com>
2025-05-06 21:28:55 +0530

    Updates

--- build_routine.sh
+++ build_routine.sh
@@ -1,4 +1,6 @@
 # Build HTML files using asciidoctor
+asciidoctor -o index.html README.adoc
+
 asciidoctor -o ./build/html/findNeedles.html ./src/findNeedles.adoc
 asciidoctor -o ./build/html/TicTacToe.html ./src/TicTacToe.adoc
 asciidoctor -o ./build/html/RESTvsSOAP.html ./src/RESTvsSOAP.adoc
@@ -6,9 +8,8 @@ asciidoctor -o ./build/html/debug_pods_kubernetes.html ./src/debug_pods_kubernet
 asciidoctor -o ./build/html/cloud_vision.html ./src/cloud_vision.adoc
 
 # Build pdf files using asciidoctor-pdf
-# asciidoctor -r asciidoctor-pdf -b pdf -o ./build/pdf/exercises/findNeedles.pdf ./src/findNeedles.adoc
-# asciidoctor -r asciidoctor-pdf -b pdf -o ./build/pdf/exercises/RESTvsSOAP.pdf ./src/RESTvsSOAP.adoc
-# asciidoctor -r asciidoctor-pdf -b pdf -o ./build/pdf/exercises/TicTacToe.pdf ./src/TicTacToe.adoc
-
-# Move TicTacToe.pdf to the samples folder
-# mv ./build/pdf/exercises/TicTacToe.pdf ./build/pdf/samples/TicTacToe.pdf
+asciidoctor -r asciidoctor-pdf -b pdf -o ./build/pdf/findNeedles.pdf ./src/findNeedles.adoc
+asciidoctor -r asciidoctor-pdf -b pdf -o ./build/pdf/TicTacToe.pdf ./src/TicTacToe.adoc
+asciidoctor -r asciidoctor-pdf -b pdf -o ./build/pdf/RESTvsSOAP.pdf ./src/RESTvsSOAP.adoc
+asciidoctor -r asciidoctor-pdf -b pdf -o ./build/pdf/debug_pods_kubernetes.pdf ./src/debug_pods_kubernetes.adoc
+asciidoctor -r asciidoctor-pdf -b pdf -o ./build/pdf/cloud_vision.pdf ./src/cloud_vision.adoc
--- git_routine.sh
+++ git_routine.sh
@@ -1,3 +1,5 @@
+git_routine.sh
+
 # Git routine
 git add .
 git commit -m "Updates"
