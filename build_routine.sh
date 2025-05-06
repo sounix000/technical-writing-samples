41ea25ebb55b49fc39fabe4d2aba17515a6600e5
Souvik Sarkar <souvik.sarkar@suse.com>
2025-05-06 22:16:41 +0530

    Updates

--- build_routine.sh
+++
@@ -1,8 +0,0 @@
-# Build HTML files using asciidoctor
-asciidoctor -o index.html README.adoc
-
-asciidoctor -o ./build/html/findNeedles.html ./src/findNeedles.adoc
-asciidoctor -o ./build/html/TicTacToe.html ./src/TicTacToe.adoc
-asciidoctor -o ./build/html/RESTvsSOAP.html ./src/RESTvsSOAP.adoc
-asciidoctor -o ./build/html/debug_pods_kubernetes.html ./src/debug_pods_kubernetes.adoc
-asciidoctor -o ./build/html/cloud_vision.html ./src/cloud_vision.adoc
--- git_routine.sh
+++ git_routine.sh
@@ -1,3 +1,12 @@
+
+# Build HTML files using asciidoctor
+asciidoctor -o index.html README.adoc
+asciidoctor -o ./build/html/findNeedles.html ./src/findNeedles.adoc
+asciidoctor -o ./build/html/TicTacToe.html ./src/TicTacToe.adoc
+asciidoctor -o ./build/html/RESTvsSOAP.html ./src/RESTvsSOAP.adoc
+asciidoctor -o ./build/html/debug_pods_kubernetes.html ./src/debug_pods_kubernetes.adoc
+asciidoctor -o ./build/html/cloud_vision.html ./src/cloud_vision.adoc
+
 # Git routine
 git add .
 git commit -m "Updates"
